9eef02ff997344d3f7d6043e10602d25ce368c70
Eric Schneider <u1643364@live.unc.edu>
2021-01-08 05:55:17 -0500

    Working on a chart for my lang experience

--- scripts.js
+++ scripts.js
@@ -1,5 +1,53 @@
 $(document).ready(() => {
-	// Contact
+	/// Chart
+	let ctx = document.getElementById("techChart").getContext('2d');
+
+	new Chart(ctx, {
+		type: 'bar',
+		data: {
+			labels: [
+				'January',
+				'February',
+				'March',
+				'April',
+				'May',
+				'June',
+				'July'
+			],
+			datasets: [{
+				label: 'My First Dataset',
+				data: [65, 59, 80, 81, 56, 55, 40],
+				backgroundColor: [
+					'rgba(255, 99, 132, 0.2)',
+					'rgba(255, 159, 64, 0.2)',
+					'rgba(255, 205, 86, 0.2)',
+					'rgba(75, 192, 192, 0.2)',
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(153, 102, 255, 0.2)',
+					'rgba(201, 203, 207, 0.2)'
+				],
+				borderColor: [
+					'rgb(255, 99, 132)',
+					'rgb(255, 159, 64)',
+					'rgb(255, 205, 86)',
+					'rgb(75, 192, 192)',
+					'rgb(54, 162, 235)',
+					'rgb(153, 102, 255)',
+					'rgb(201, 203, 207)'
+				],
+				borderWidth: 1
+			}]
+		},
+		options: {
+			scales: {
+				y: {
+					beginAtZero: true
+				}
+			}
+		}
+	});
+
+	/// Contact
 	$('a.copyemail').click(() => {
 		let emailAddress = "u164" + "" + "33" + "64@live" + "." + "unc.edu";
 
@@ -18,6 +66,16 @@ $(document).ready(() => {
 			$('.refresh-button').css('display', 'block');
 		});
 	});
+
+	function addFilter(buttonClass, classToKeep){
+		$(buttonClass).click(() => {
+			$('.show-just-active-buttons').fadeOut(400, 'swing', () => {
+				$('.proj-active, .proj-inactive, .show-just-active-buttons').css('display', 'none');
+				$(classToKeep).css('display', '');
+				$('.refresh-button').css('display', 'block');
+			});
+		});
+	}
 
 	// Show only Java
 	addFilter('.button-java', '.proj-java');
@@ -60,13 +118,3 @@ $(document).ready(() => {
 		});
 	});
 });
-
-function addFilter(buttonClass, classToKeep){
-	$(buttonClass).click(() => {
-		$('.show-just-active-buttons').fadeOut(400, 'swing', () => {
-			$('.proj-active, .proj-inactive, .show-just-active-buttons').css('display', 'none');
-			$(classToKeep).css('display', '');
-			$('.refresh-button').css('display', 'block');
-		});
-	});
-}
