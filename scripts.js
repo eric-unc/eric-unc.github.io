6b6ef21ec13024539f17c1f3b7d8f84c55a647b0
Eric Schneider <u1643364@live.unc.edu>
2021-01-07 23:04:57 -0500

    Cleanup, some animation

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
-$(document).ready(function(){
+$(document).ready(() => {
 	// Contact
-	$('a.copyemail').click(function(){
+	$('a.copyemail').click(() => {
 		let emailAddress = "u164" + "" + "33" + "64@live" + "." + "unc.edu";
 
-		navigator.clipboard.writeText(emailAddress).then(function(){
+		navigator.clipboard.writeText(emailAddress).then(() => {
 			// Successfully copied.
 		}, function(){
   			console.error("Unable to write to clipboard!");
@@ -11,13 +11,17 @@ $(document).ready(function(){
 	});
 
 	// Project filters
-	$('button.show-just-active-button').click(function(){
-		$('li.proj-inactive, button.show-just-active-button').css('display', 'none');
-		$('button.refresh-button').css('display', 'block');
+	$('button.show-just-active-button').click(() => {
+		$('li.proj-inactive, button.show-just-active-button').fadeOut(400, 'swing', () => {
+			$('li.proj-inactive, button.show-just-active-button').css('display', 'none');
+			$('button.refresh-button').css('display', 'block');
+		});
 	});
 
-	$('button.refresh-button').click(function(){
-		$('button.refresh-button').css('display', 'none');
-		$('li.proj-inactive, button.show-just-active-button').css('display', '');
+	$('button.refresh-button').click(() => {
+		$('li.proj-inactive, button.show-just-active-button').fadeOut(400, 'swing', () => {
+			$('button.refresh-button').css('display', 'none');
+			$('li.proj-inactive, button.show-just-active-button').css('display', '');
+		});
 	});
 });
