27b8bb94b145f73554a8c91f6a2397079e0e5ca2
Eric Schneider <u1643364@live.unc.edu>
2021-01-07 22:37:17 -0500

    Big refactor

--- scripts.js
+++ scripts.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
+	// Contact
+	$('a.copyemail').click(function(){
+		let emailAddress = "u164" + "" + "33" + "64@live" + "." + "unc.edu";
+
+		navigator.clipboard.writeText(emailAddress).then(function(){
+			// Successfully copied.
+		}, function(){
+  			console.error("Unable to write to clipboard!");
+		});
+	});
+
+	// Project filters
 	$('button.show-just-active-button').click(function(){
 		$('li.proj-inactive, button.show-just-active-button').css('display', 'none');
 		$('button.refresh-button').css('display', 'block');
