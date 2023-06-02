461ec024422a365fc208e52ffd742afc48ff0a71
Eric Schneider <u1643364@live.unc.edu>
2023-06-02 01:33:01 -0400

    pretty WIP

---
+++ pages/personal.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <section class="hero is-info is-bold">
+      <div class="hero-head">
+        <Navbar />
+      </div>
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">Personal</h1>
+        </div>
+      </div>
+    </section>
+    <section class="section">
+      <div class="content">
+        <p>I (Eric Thomas Schneider) was born on May 1st, 2000, in Charlotte, North Carolina to Deborah and Paul Schneider. I was raised in Weddington, North Carolina, attending Weddington Elementary School, Weddington Middle School, and Weddington High School.</p>
+        <p>I recently (May 2023) graduated from UNC Chapel Hill with a Bachelors of Science in Computer Science and Mathematics. I am starting my Masters of Science in Computer Science in fall 2023.</p>
+        <p>I enjoy (casual) bodybuilding and long distance (marathon/half) running. I've run 7 marathons, with <a href="https://results.raceroster.com/v2/en-US/results/g39ey4js9ncsxz5r/detail/bwqy2xucqy75gpt4">my fastest being 3:10:32</a>. I don't measure maxs, but I like to pretend I can bench two plates (225 lb), and squat/deadlift more than that (hard to guess because of injuries).</p>
+        <p>I occasionally like to play video games, though I'm not very good at them. My favorites are Minecraft, Cube 2: Sauerbraten, and Bloons Tower Defense 6. Modded Minecraft is actually what got me into programming and thus computer science (see "Projects").</p>
+        <p>I am interested in international politics and affairs, and I keep up with world news closely. I like cats, and love my parents' cats, Trixie and Scout.</p>
+      </div>
+    </section>
+    <section class="section">
+      <div class="content">
+        <h2 class="subtitle">Gallery</h2>
+        <p>idk, I'll put something here later.</p>
+      </div>
+    </section>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
