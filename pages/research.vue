aed067033da8b7087f8b88a627cff778e5bcee02
Eric Schneider <u1643364@live.unc.edu>
2023-06-04 02:27:08 -0400

    Very WIP

---
+++ pages/research.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <section class="hero is-info is-bold">
+      <div class="hero-head">
+        <Navbar />
+      </div>
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">Teaching</h1>
+        </div>
+      </div>
+    </section>
+    <section class="section">
+      <div class="content">
+        <p>I have uh done research</p>
+        <ul>
+          <li><strong>ADΔER</strong>.</li>
+          <li><strong>Gramine</strong>. I have contributed to the yet-to-be-published paper <em>Improving SGX Performance with Cooperative, Dynamic Page Management</em>, working with Professor Donald Porter. Mostly just with writing.</li>
+        </ul>
+      </div>
+    </section>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
---
+++ pages/teaching.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <section class="hero is-info is-bold">
+      <div class="hero-head">
+        <Navbar />
+      </div>
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">Teaching</h1>
+        </div>
+      </div>
+    </section>
+    <section class="section">
+      <div class="content">
+        <p>I have served as an undergraduate teaching assistant in the UNC computer science department for 6 semesters. These are the classes I have been a UTA for:</p>
+        <ul>
+          <li><strong>System Fundamentals (COMP 211, spring 2023)</strong>, under Professor Brent Munsell.</li>
+          <li><strong>Operating Systems (COMP 530, fall 2023)</strong>, under Professor Don Porter.</li>
+          <li><strong>System Fundamentals (COMP 211, spring 2022)</strong>, under Professor Ketan Mayer-Patel.</li>
+          <li><strong>System Fundamentals (COMP 211, fall 2021)</strong>, under Professor Brent Munsell. This semester, I was promoted to "lead LA", managing labs for the course. I created my own lab, the MIPS simulator lab, which has still been used to this day.</li>
+          <li><strong>System Fundamentals (COMP 211, spring 2021)</strong>, under Professor Brent Munsell.</li>
+          <li><strong>System Fundamentals (COMP 211, fall 2020)</strong>, under Professor Kris Jordan.</li>
+        </ul>
+      </div>
+    </section>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
